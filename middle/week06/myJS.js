30c3e555737693058028c6a61e7e999f08d9777f
최시창 <64635593+choitest123@users.noreply.github.com>
2020-10-28 01:15:26 +0900

    MyStudy Up

---
+++ middle/week06/myJS.js
@@ -0,0 +1,5 @@
+	function myFun(){//내부 스타일 시트 
+			document.bgColor ='pink';
+			document.getElementById("demo").innerHTML = "동의대학교 최시창입니다"; //이름에 해당하는 엘리먼트 ID에 해당하는 내용을 삽입해서 속성을 바꿈 
+			// 엘리먼트 안에 삽입해 출력하는 형태 innerHTML 여기서는 ID demo를 찾아 값을 넣음 html 내부에다가 넣겠다는 뜻 
+		}// 블럭
